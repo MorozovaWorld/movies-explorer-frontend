da1dd16c5ba49f6522b1cadef955c7ad7d36c427
Ekaterina Morozova <emorozova1@yandex.ru>
2021-06-08 21:24:35 +0300

    Responsive layout for the /movies route - part 1: checkbox, searchform, moviesCard

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,23 @@
 import accountIcon from '../../images/account-icon.svg';
-import { Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Route, Switch, Link, NavLink, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function Navigation() {
+  const [moviesBtnActive, setMoviesBtnActive] = useState(true);
+  const [savedMoviesBtnActive, setSavedMoviesBtnActive] = useState(false);
+  const location = useLocation();
+
+  React.useEffect(() => {
+    if(location.pathname === '/movies') {
+      setMoviesBtnActive(true);
+      setSavedMoviesBtnActive(false);
+    } 
+    if(location.pathname === '/saved-movies') {
+      setMoviesBtnActive(false);
+      setSavedMoviesBtnActive(true);
+    }
+  }, [location]);
+  
   return (
     <nav className="header__navigation">
       <Switch>
@@ -11,10 +27,10 @@ function Navigation() {
         </Route>
         <Route path={["/movies", "/saved-movies", "/profile"]} >
           <div className="header__navigation-movies">
-            <NavLink className="header__navigation-navBtn opacity opacity_useAt_link" to="/movies" >Фильмы</NavLink>
-            <NavLink className="header__navigation-navBtn header__navigation-navBtn_wight_400 opacity opacity_useAt_link" to="/saved-movies" >Сохранённые фильмы</NavLink>
+            <NavLink className={"header__navigation-navBtn header__navigation-navBtn_link-to_movies opacity opacity_useAt_link" + (moviesBtnActive ? ' header__navigation-navBtn_status_active' : '')}  to="/movies" >Фильмы</NavLink>
+            <NavLink className={"header__navigation-navBtn header__navigation-navBtn_link-to_movies opacity opacity_useAt_link"  + (savedMoviesBtnActive ? ' header__navigation-navBtn_status_active' : '')} to="/saved-movies" >Сохранённые фильмы</NavLink>
           </div>
-            <NavLink className="header__navigation-navBtn opacity opacity_useAt_link" to="/profile" >Аккаунт</NavLink>
+            <NavLink className="header__navigation-navBtn header__navigation-navBtn_link-to_account opacity opacity_useAt_link" to="/profile" >Аккаунт</NavLink>
             <Link className="header__navigation-accountIcon opacity opacity_useAt_link" to="/profile" >
               <img src={accountIcon} alt="логотип сайта" />
             </Link>
