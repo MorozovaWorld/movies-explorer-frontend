c62782afbe882ad839bb59a7e903210f73f54956
Ekaterina Morozova <emorozova1@yandex.ru>
2021-06-21 00:21:05 +0300

    feat: form validation part1

--- src/components/FormPage/FormPage.js
+++ src/components/FormPage/FormPage.js
@@ -1,18 +1,18 @@
 import { Link, } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 
-function FormPage({title, children, btnText, linkText, linkUrl, linkReason }) {
+function FormPage({title, children, btnText, linkText, linkUrl, linkReason, isDisabled }) {
   return (
     <>
       <form className="formPage" name="name">
-        <Link className="formPage__logo opacity opacity_useAt_link" to="/" >
-          <img src={logo} alt="логотип сайта" />
-        </Link>
+        <div className="formPage__logo opacity opacity_useAt_link">
+          <Link to="/" ><img src={logo} alt="логотип сайта" /></Link>
+        </div>
         <fieldset className="formPage__fieldset">
           <legend className="formPage__title">{title}</legend>
           {children}
         </fieldset>
-        <button type="submit" className="formPage__button-submit opacity opacity_useAt_button">{btnText}</button>
+        <button type="submit" disabled={isDisabled} className={"formPage__button-submit opacity opacity_useAt_button" + (!isDisabled ? '   formPage__button-submit_active' : '')}>{btnText}</button>
         <p className="formPage__bottomText">{linkReason}<Link to={linkUrl} className="formPage__link opacity opacity_useAt_button">{linkText}</Link></p>
       </form>
     </>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import FormPage from '../FormPage/FormPage.js';
 import FormSubmitErr from '../FormSubmitErr/FormSubmitErr.js';
+import {validators} from '../../utils/constants.js'
 
 function Register() {
   const [userData, setUserData] = useState({
@@ -8,31 +9,90 @@ function Register() {
     email: '',
     password: '',
   });
+  const {name, email, password} = userData;
 
-  function handleChange(e) {
+  const handleChange = useCallback((e) => {
     const { name, value } = e.target;
 
-    setUserData({
+    setUserData(userData => ({
       ...userData,
       [name]: value
+    }));
+  }, [setUserData]);
+
+  const [validationErrors, setValidationErrors] = useState ({
+    name: {
+      required: false,
+      minlength: false,
+      maxlength: false,
+    },
+    email: {
+      required: false,
+      isEmail: false,
+    },
+    password: {
+      required: false,
+    },
+  });
+
+  useEffect(function validateInputs() {
+    const {name, email, password} = userData;
+
+    const nameValidationResult = Object.keys(validators.name).map((errorKey) => {
+      const errorResult = validators.name[errorKey](name);
+
+      return { [errorKey]: errorResult }
+    }).reduce((acc, el) => ({ ...acc, ...el }), []);
+
+    const emailValidationResult = Object.keys(validators.email).map((errorKey) => {
+      const errorResult = validators.email[errorKey](email);
+
+      return { [errorKey]: errorResult }
+    }).reduce((acc, el) => ({ ...acc, ...el }), []);
+
+    const passwordValidationResult = Object.keys(validators.password).map((errorKey) => {
+      const errorResult = validators.name[errorKey](password);
+
+      return { [errorKey]: errorResult }
+    }).reduce((acc, el) => ({ ...acc, ...el }), []);
+
+    setValidationErrors({
+      name: nameValidationResult,
+      email: emailValidationResult,
+      password: passwordValidationResult,
     })
-  }
+  }, [userData, setValidationErrors]);
+
+  const isNameValid = Object.values(validationErrors.name).some(Boolean);
+  const isEmailValid = Object.values(validationErrors.email).some(Boolean);
+  const isPasswordValid = Object.values(validationErrors.password).some(Boolean);
+  const isSubmitDisabled = (isNameValid || isEmailValid || isPasswordValid);
 
   return (
-    <FormPage title='Добро пожаловать!' btnText="Зарегистрироваться" linkText="Войти" linkReason="Уже зарегистрированы?" linkUrl="/signin">
+    <FormPage title='Добро пожаловать!' btnText="Зарегистрироваться" linkText="Войти" linkReason="Уже зарегистрированы?" linkUrl="/signin" isDisabled={isSubmitDisabled}>
       <div className="formPage__input-container">
         <label htmlFor="user-name" className="formPage__input-label">Имя</label>
-        <input type="text" onChange={handleChange} name='name' value={userData.name} id="user-name" className="formPage__input-text" placeholder="Введите имя" autoComplete='off' required />
-        <span id="name-error" className="formPage__input-error"></span>
+        <input type="text" onChange={handleChange} name='name' value={name} id="user-name" className="formPage__input-text" placeholder="Введите имя" autoComplete='off' required />
+        <span id="name-error" className="formPage__input-error">
+          {validationErrors.name.required && 'Поле не заполнено'}
+          {!validationErrors.name.required && validationErrors.name.minlength && 'Минимальное количество символов: 2'}
+          {validationErrors.name.maxlength && 'Максимальное количество символов: 30'}
+        </span>
       </div>
       <div className="formPage__input-container">
         <label htmlFor="user-email" className="formPage__input-label">E-mail</label>
-        <input type="email" onChange={handleChange} placeholder="Введите e-mail" name='email' value={userData.email} id="user-email" className="formPage__input-text" autoComplete='off' required />
-        <span id="email-error" className="formPage__input-error"></span>
+        <input type="email" onChange={handleChange} placeholder="Введите e-mail" name='email' value={email} id="user-email" className="formPage__input-text" autoComplete='off' required />
+        <span id="email-error" className="formPage__input-error">
+          {validationErrors.email.required && 'Поле не заполнено'}
+          {!validationErrors.email.required && validationErrors.email.isEmail && 'Введите, пожалуйста, email'}
+        </span>
       </div>
       <div className="formPage__input-container">
         <label htmlFor="user-password" className="formPage__input-label">Пароль</label>
-        <input type="password" onChange={handleChange} name='password' value={userData.password} autoComplete='off' id="user-password" className="formPage__input-text" required />
+        <input type="password" onChange={handleChange} name='password' value={password} autoComplete='off' id="user-password" className="formPage__input-text" required />
+        <span id="email-error" className="formPage__input-error">
+          {validationErrors.password.required && 'Поле не заполнено'}
+        </span>
         <FormSubmitErr errText=''></FormSubmitErr>
       </div>
     </FormPage>
