569f92a54ef1846e7f4a4165d603211f6f5b3635
Ekaterina Morozova <emorozova1@yandex.ru>
2021-06-21 22:12:06 +0300

    feat: register form validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,6 +20,22 @@ function Register() {
     }));
   }, [setUserData]);
 
+  // стейт фокуса инпутов для регуляции первичного рендера ошибок валидации,
+  // если в фокусе – начать отображение ошибок
+  const [isInputFocused, setInputFocused] = useState ({
+    name: false,
+    email: false,
+    password: false,
+  });
+
+  const handleInputFocus = (e) => {
+    const { name } = e.target;
+
+    setInputFocused({ ...isInputFocused,
+      [name]: true
+    })
+  }
+
   const [validationErrors, setValidationErrors] = useState ({
     name: {
       required: false,
@@ -35,6 +51,7 @@ function Register() {
     },
   });
 
+  // моментальная валидация инпутов и кнопки сабмита
   useEffect(function validateInputs() {
     const {name, email, password} = userData;
 
@@ -63,35 +80,41 @@ function Register() {
     })
   }, [userData, setValidationErrors]);
 
-  const isNameValid = Object.values(validationErrors.name).some(Boolean);
-  const isEmailValid = Object.values(validationErrors.email).some(Boolean);
-  const isPasswordValid = Object.values(validationErrors.password).some(Boolean);
-  const isSubmitDisabled = (isNameValid || isEmailValid || isPasswordValid);
+  const [isFormValid, setFormValid] = useState(false);
 
+  // общая проверка валидности формы для активации кнопки сабмита
+  useEffect(function validateInputs() {
+    const isNameValid = Object.values(validationErrors.name).some(Boolean);
+    const isEmailValid = Object.values(validationErrors.email).some(Boolean);
+    const isPasswordValid = Object.values(validationErrors.password).some(Boolean);
+
+    setFormValid((isNameValid || isEmailValid || isPasswordValid));
+  }, [validationErrors.name, validationErrors.email, validationErrors.password, setFormValid])
+  
   return (
-    <FormPage title='Добро пожаловать!' btnText="Зарегистрироваться" linkText="Войти" linkReason="Уже зарегистрированы?" linkUrl="/signin" isDisabled={isSubmitDisabled}>
+    <FormPage title='Добро пожаловать!' btnText="Зарегистрироваться" linkText="Войти" linkReason="Уже зарегистрированы?" linkUrl="/signin" isDisabled={isFormValid}>
       <div className="formPage__input-container">
         <label htmlFor="user-name" className="formPage__input-label">Имя</label>
-        <input type="text" onChange={handleChange} name='name' value={name} id="user-name" className="formPage__input-text" placeholder="Введите имя" autoComplete='off' required />
+        <input type="text" onFocus={handleInputFocus} onChange={handleChange} name='name' value={name} id="user-name" className="formPage__input-text" placeholder="Введите имя" autoComplete='off' required />
         <span id="name-error" className="formPage__input-error">
-          {validationErrors.name.required && 'Поле не заполнено'}
+          {isInputFocused.name && validationErrors.name.required && 'Поле не заполнено'}
           {!validationErrors.name.required && validationErrors.name.minlength && 'Минимальное количество символов: 2'}
           {validationErrors.name.maxlength && 'Максимальное количество символов: 30'}
         </span>
       </div>
       <div className="formPage__input-container">
         <label htmlFor="user-email" className="formPage__input-label">E-mail</label>
-        <input type="email" onChange={handleChange} placeholder="Введите e-mail" name='email' value={email} id="user-email" className="formPage__input-text" autoComplete='off' required />
+        <input type="email" onFocus={handleInputFocus} onChange={handleChange} placeholder="Введите e-mail" name='email' value={email} id="user-email" className="formPage__input-text" autoComplete='off' required />
         <span id="email-error" className="formPage__input-error">
-          {validationErrors.email.required && 'Поле не заполнено'}
+          {isInputFocused.email && validationErrors.email.required && 'Поле не заполнено'}
           {!validationErrors.email.required && validationErrors.email.isEmail && 'Введите, пожалуйста, email'}
         </span>
       </div>
       <div className="formPage__input-container">
         <label htmlFor="user-password" className="formPage__input-label">Пароль</label>
-        <input type="password" onChange={handleChange} name='password' value={password} autoComplete='off' id="user-password" className="formPage__input-text" required />
+        <input type="password" onFocus={handleInputFocus} onChange={handleChange} name='password' value={password} autoComplete='off' id="user-password" className="formPage__input-text" placeholder="Введите пароль" required />
         <span id="email-error" className="formPage__input-error">
-          {validationErrors.password.required && 'Поле не заполнено'}
+          {isInputFocused.password && validationErrors.password.required && 'Поле не заполнено'}
         </span>
         <FormSubmitErr errText=''></FormSubmitErr>
       </div>
